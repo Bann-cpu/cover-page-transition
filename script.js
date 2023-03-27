20326ac501802fe1cd9bb63a91fd07e7dbda8736
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-03-27 22:27:43 +0200

    changes

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const icon_Hamburger = document.querySelector(".fa-solid")
 const listElement = document.querySelector(".list-link")
+const otherPart = document.querySelector(".OtherPart")
 
 window.onload=function(){
     if(icon_Hamburger) {
         icon_Hamburger.addEventListener('click', () => {
             console.log("ITS WORKING");
-            listElement.classList.toggle("active")
+            listElement.classList.toggle("activeSpecial")
+            otherPart.classList.toggle("activated")
         })
     }else {
         console.log("its not working");
