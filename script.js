3f45e3143f5e6073e349960ded6ad62b3dade78f
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-03-29 23:47:36 +0200

    BIG Changes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const icon_Hamburger = document.querySelector(".fa-bars");
 const overlaySecond = document.querySelector(".second_part");
 const containerItem = document.querySelector(".container");
-const overlayOne = containerItem.closest(".first_part");
+const overlayOne = document.querySelector(".first_part");
 
 if(overlayOne) {
     icon_Hamburger.addEventListener('click', () => {
-        overlayOne.classList.toggle("overlay_activated")
-        overlaySecond.classList.toggle("overlay_activated_nd")
-        containerItem.style.overflow = "hidden";
+        
+        containerItem.classList.add("no-animation");
+        overlayOne.classList.add("overlay_activated");
+        overlaySecond.classList.add("overlay_activated_nd");
     })
 }
