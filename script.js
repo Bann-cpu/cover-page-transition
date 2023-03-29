06cd27889b56aab00c4773d9fe5be6ad4bfdec59
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-03-30 01:14:09 +0200

    changes

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ const overlayOne = document.querySelector(".first_part");
 
 if(overlayOne) {
     icon_Hamburger.addEventListener('click', () => {
-        
-        containerItem.classList.add("no-animation");
-        overlayOne.classList.add("overlay_activated");
+        overlayOne.classList.add("overlay_activated");  
         overlaySecond.classList.add("overlay_activated_nd");
     })
 }
