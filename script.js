fcd0574f9b4b7d9a8176f504f50e257ed9d53604
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-03-30 21:37:15 +0200

    opacity is the answer

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const icon_Hamburger = document.querySelector(".fa-bars");
 const overlaySecond = document.querySelector(".second_part");
-const containerItem = document.querySelector(".container");
 const overlayOne = document.querySelector(".first_part");
+const imageBack = document.querySelector(".image_background")
 
 if(overlayOne) {
     icon_Hamburger.addEventListener('click', () => {
         overlayOne.classList.add("overlay_activated");  
         overlaySecond.classList.add("overlay_activated_nd");
+        imageBack.classList.add("imageActivated");
     })
 }
